b23a99b3c3417357a19e8029d8939410e1c2a3c2
Ethan Kiang <chocopuff298@gmail.com>
2020-08-05 20:11:40 +0800

    Added Show instance to POSTag

--- src/Dictionary.hs
+++ src/Dictionary.hs
@@ -15,7 +15,39 @@ data POSTag
   | M | Q | R | P
   | C | U | XC | W
   | PER | LOC | ORG | TIME
-  | Untagged deriving (Show)
+  | Untagged
+
+instance Show POSTag where
+  show posTag = case posTag of
+    N -> "n 普通名詞"
+    F -> "f 方位名詞"
+    S -> "s 處所名詞"
+    T -> "t 時間"
+    NR -> "nr 人名"
+    NS -> "ns 地名"
+    NT -> "nt 機構名"
+    NW -> "nw 作品名"
+    NZ -> "nz 其他專名"
+    V -> "v 普通動詞"
+    VD -> "vd 動副詞"
+    VN -> "vn 名動詞"
+    A -> "a 形容詞"
+    AD -> "ad 副形詞"
+    AN -> "an 名形詞"
+    D -> "d 副詞"
+    M -> "m 數量詞"
+    Q -> "q 量詞"
+    R -> "r 代詞"
+    P -> "p 介詞"
+    C -> "c 連詞"
+    U -> "u 助詞"
+    XC -> "xc 其他虛詞"
+    W -> "w 標點符號"
+    PER -> "PER 人名"
+    LOC -> "LOC 地名"
+    ORG -> "ORG 機構名"
+    TIME -> "TIME 時間"
+    Untagged -> "無標籤"
 
 data Entry = Entry { freq :: Frequency, pos :: POSTag } deriving (Show)
 type DictEntryPair = (Term, Entry)
