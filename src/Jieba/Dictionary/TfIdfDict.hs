fe0385cb4446921e7499b40a2ae9104df18bd27f
Ethan Kiang <chocopuff298@gmail.com>
2020-08-06 12:20:50 +0800

    Refactored TfIdfDict to use higher ord fn from Dictionary

--- src/Jieba/Dictionary/TfIdfDict.hs
+++ src/Jieba/Dictionary/TfIdfDict.hs
@@ -1,25 +1,18 @@
 module Jieba.Dictionary.TfIdfDict where
 
 import Jieba.Dictionary as D
-import qualified Data.Map.Strict as Map
 
 type TfIdf = Double
 type TfIdfDict = Dictionary TfIdf Empty
-type DictEntryPair = (String, TfIdf)
+type DictEntryPair = EntryPair TfIdf
 
 data Empty = Empty
 
-entryPairFromContents :: String -> [DictEntryPair]
-entryPairFromContents input = map f $ lines input
-  where
-    f = words2entry . words
-    words2entry xs = (term, tfidf)
-      where
-        term = head xs
-        tfidf = read (xs !! 1) :: TfIdf
+words2entry :: [String] -> DictEntryPair
+words2entry xs = (head xs, read (xs !! 1) :: TfIdf)
 
-dictFromEntryPairs :: [DictEntryPair] -> TfIdfDict
-dictFromEntryPairs entryPairs = Dictionary (Map.fromList entryPairs) Empty
+dictFromContents :: String -> TfIdfDict
+dictFromContents = dictInputParser words2entry (const Empty)
 
 lookupTfIdf :: TfIdfDict -> String -> Maybe TfIdf
 lookupTfIdf = D.lookup
