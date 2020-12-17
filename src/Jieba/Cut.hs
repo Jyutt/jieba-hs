36b488bdafe368d870ba15b6deca6287b9086355
Ethan Kiang <chocopuff298@gmail.com>
2020-12-17 21:57:59 +0800

    Switch to DLists for more efficient appends

--- src/Jieba/Cut.hs
+++ src/Jieba/Cut.hs
@@ -1,42 +1,40 @@
-{-# LANGUAGE MultiWayIf #-}
-
 module Jieba.Cut where
 
-import Jieba.Graph as Graph
+import qualified Jieba.Graph as G
 import Jieba.Types.PosTag
 import Jieba.Dictionary.FreqDict as FD
-import Jieba.Dictionary.HmmDict
-import Control.Monad.State.Lazy
-import Jieba.FinalSeg as FS
-
-import Data.Array -- cutAll
+import Jieba.Dictionary.HmmDict as HD
+import Data.Array ((!), bounds)
+import qualified Jieba.FinalSeg as FS
+import qualified Data.DList as DL
 
 cutNoHMM :: FreqDict -> String -> [String]
-cutNoHMM dict snt = seg . followPath . calcOptimalPath dict $ dag
+cutNoHMM dict snt = seg . G.followPath . G.calcOptimalPath dict $ dag
   where
-    dag = buildDAG dict snt
-    seg = segmentSentence snt
+    dag = G.buildDAG dict snt
+    seg = G.segmentSentence snt
 
 cutHMM :: FreqDict -> HmmDict -> String -> [String]
-cutHMM fd hd snt = cutHMM' firstCut []
+cutHMM fd hd snt = DL.toList . cutHMM' firstCut $ mempty
   where firstCut = cutNoHMM fd snt
         cutHMM' rem acc
           | null rem = acc
-          | length buff == 0 = cutHMM' rem (acc ++ seg)
-          | length buff == 1 = cutHMM' rem'' (acc ++ buff ++ seg)
-          | FD.entryExists fd (concat buff) = cutHMM' rem'' (acc ++ buff ++ seg)
-          | otherwise = cutHMM' rem'' (acc ++ fs ++ seg)
-          where buff = takeWhile ((==1) . length) rem
-                rem' = dropWhile ((==1) . length) rem
+          | length buff == 0 = cutHMM' rem (acc <> seg)
+          | length buff == 1 = cutHMM' rem'' (acc <> dlBuff <> seg)
+          | FD.entryExists fd buff' = cutHMM' rem'' (acc <> dlBuff <> seg)
+          | otherwise = cutHMM' rem'' (acc <> fs <> seg)
+          where (buff, rem') = span ((== 1) . length) rem
+                buff' = concat buff
+                dlBuff = DL.fromList buff
                 rem'' = drop 1 rem'
-                seg = take 1 rem'
-                fs = FS.finalseg hd (concat buff)
+                seg = DL.fromList . take 1 $ rem'
+                fs = DL.fromList . FS.finalseg hd $ buff'
 
 cutAll :: FreqDict -> String -> [String]
 cutAll dict snt = concatMap f idxs
   where
-    dag = buildDAG dict snt
+    dag = G.buildDAG dict snt
     (_, n) = bounds dag -- first idx is always 0
     idxs = [0..n]
     substr x y = take (y - x + 1) . drop x $ snt
-    f ix = map (substr ix . vertex) (dag ! ix)
+    f ix = map (substr ix . G.vertex) (dag ! ix)
--- src/Main.hs
+++ src/Main.hs
@@ -9,14 +9,10 @@ import Data.List (intercalate)
 
 main :: IO ()
 main = do
-    --contents <- hGetContents =<< openFile "data/dict.txt.small" ReadMode
-    --let dict = dictFromContents contents
     dict <- readFreqDict "data/dict.txt.small"
+    hmmd <- readHmmDict "data/hmm.model"
     let snt = "他来到了网易杭研大厦"
     let result = cutNoHMM dict snt
+    let result' = cutHMM dict hmmd snt
     putStrLn $ intercalate "/" result
-    hmmd <- readHmmDict "data/hmm.model"
-    let snt = "这是一个HMM的测试"
-    let states = viterbi hmmd snt
-    let split = segment snt states
-    putStrLn . intercalate "/" $ split
+    putStrLn . intercalate "/" $ result'
